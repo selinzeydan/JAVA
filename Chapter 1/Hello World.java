1fe78db992c87a9b34525a5f0b982eba0f974244
Selin Zeydan <116116043+selinzeydan@users.noreply.github.com>
2022-12-05 10:47:09 +0300

    Create Hello World.java

---
+++ Chapter 1/Hello World.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+  public static void main(String[] args) {
+    
+    //Display massage Welcome to Java! - Hello World! on the console
+    System.out.println("Welcome to Java! - Hello World!");
+  }
+}
